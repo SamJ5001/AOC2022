239e60831f95a6af1ff939c8c48d2d2f7235c9c3
Sam Jones <samuel.d.jones5001@gmail.com>
2022-12-08 11:29:52 +0000

    Init project

---
+++ AOC22/Project1/Day1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC22
+{
+     class Day1
+    {
+        static void Main(string[] args)
+
+        {
+            
+
+
+
+
+        }
+    }
+}
