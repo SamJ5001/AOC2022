8c00953651fdde0545b8d2382ff2fcf8fc5f6fb7
Sam Jones <samuel.d.jones5001@gmail.com>
2022-12-15 15:23:36 +0000

    4B

--- AOC22/Day4.cs
+++ AOC22/Day4.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,9 +11,13 @@ namespace AOC22
 {
     class Day4
     {
+
+
+
         public void Day4Calc()
         {
-            int shareCount = 0;
+            int shareCountA = 0;
+            int shareCountB = 0;
             char[] separators = { ',', '-' };
 
             InputReader inputReader = new InputReader();
@@ -23,14 +28,22 @@ namespace AOC22
                 string[] str = line.Split(separators);
                 int[] pos = Array.ConvertAll(str, s => int.Parse(s));
 
+                // 3A 
                 if ((pos[0] <= pos[2] && pos[1] >= pos[3]) || (pos[0] >= pos[2] && pos[1] <= pos[3]))
                 {
-                    shareCount++;
-              //      Console.WriteLine("{0}-{1} or {2}-{3} contain one another. Share count is now {4}", pos[0], pos[1], pos[2], pos[3], shareCount);
+                    shareCountA++;
                 }
+                shareCountB += FindOverlap(pos[0], pos[1], pos[2], pos[3]);
             }
+            Console.WriteLine("\nDay 4A Calc: " + shareCountA);
+            Console.WriteLine("Day 4B Calc: " + shareCountB);
+        }
+        public int FindOverlap(int start1, int end1, int start2, int end2)
+        {
+            var find = Math.Max(0, Math.Min(end1, end2) - Math.Max(start1, start2) + 1);
 
-            Console.WriteLine("\nDay 4A Calc: " + shareCount);
+            if (find == 0) return 0;
+            else return 1;
         }
     }
 }
